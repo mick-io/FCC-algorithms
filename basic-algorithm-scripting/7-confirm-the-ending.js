f1c2b1c2cdd9f4c7b746c9cd353251ea8dce01dc
Mick Piereder <mpiereder@Micks-MacBook-Pro.local>
2017-08-06 19:27:28 -0600

    started cofirm the ending

---
+++ basic-algorithm-scripting/7-confirm-the-ending.js
@@ -0,0 +1,32 @@
+/*
+Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+String.prototype.substr()
+String.prototype.substring()
+*/
+
+function confirmEnding(str, target) {
+    var len = target.length();
+    var output = false;
+    var startIndex = target.length - len;
+    if (str.substr(startIndex) == target) {
+        output = true;
+    }
+
+    return output;
+}
+
+confirmEnding("Bastian", "n"); // should return true.
+confirmEnding("Connor", "n"); // should return false.
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification"); // should return false.
+confirmEnding("He has to give me a new name", "name"); // should return true.
+confirmEnding("Open sesame", "same"); // should return true.
+confirmEnding("Open sesame", "pen"); // should return false.
+confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain"); // should return false.
+// Do not use the built-in method .endsWith() to solve the challenge.
