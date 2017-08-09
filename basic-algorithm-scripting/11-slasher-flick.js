19a8234ecb1b48c593952b3baf526f31195521ef
Michael Piereder <I21414@xactware.com>
2017-08-09 13:51:45 -0600

    added slasher flick

---
+++ basic-algorithm-scripting/11-slasher-flick.js
@@ -0,0 +1,27 @@
+/*
+Return the remaining elements of an array after chopping off n elements from the head.
+
+The head means the beginning of the array, or the zeroth index.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Array.prototype.slice()
+Array.prototype.splice()
+*/
+
+function slasher(arr, howMany) {
+    // it doesn't always pay to be first
+    return arr;
+}
+
+slasher([1, 2, 3], 2);
+
+// test
+slasher([1, 2, 3], 2) // should return [3].
+slasher([1, 2, 3], 0) // should return [1, 2, 3].
+slasher([1, 2, 3], 9) // should return [].
+slasher([1, 2, 3], 4) // should return [].
+slasher(["burgers", "fries", "shake"], 1) // should return ["fries", "shake"].
+slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5) // should return ["cheese", 4].
