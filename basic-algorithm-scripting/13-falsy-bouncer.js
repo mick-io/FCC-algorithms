4a02318dd7021e851b85ba7fd4430173c2f2b731
Mick Piereder <mpiereder@Micks-MacBook-Pro.local>
2017-08-17 16:43:30 -0600

    started falsy bouncer

---
+++ basic-algorithm-scripting/13-falsy-bouncer.js
@@ -0,0 +1,27 @@
+/*
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Boolean Objects
+Array.prototype.filter()
+*/
+
+
+function bouncer(arr) {
+    output = arr.filter(function(false){
+        return arr
+    });
+
+    return output;
+  }
+  
+  // Test
+  bouncer([7, "ate", "", false, 9]) // should return [7, "ate", 9].
+  bouncer(["a", "b", "c"]) // should return ["a", "b", "c"].
+  bouncer([false, null, 0, NaN, undefined, ""]) // should return [].
+  bouncer([1, null, NaN, 2, undefined]) // should return [1, 2].
