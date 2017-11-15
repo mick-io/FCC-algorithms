c20da8070e173e77afbb47f5587c34a89d1206e0
Michael Piereder <I21414@xactware.com>
2017-11-15 15:29:38 -0700

    finished

--- intermediate-algorithm-scripting/3-roman-numeral-converter.js
+++ intermediate-algorithm-scripting/3-roman-numeral-converter.js
@@ -14,35 +14,62 @@ Array.prototype.join()
 */
 
 
-function convertToRoman(num) {
-    return num;
+function convertToRoman(input) {
+    var output = "";
+    var romanNumerals = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    var romanCharacters = Object.keys(romanNumerals);
+    var char = "";
+    var number = 0;
+
+    for (var i = 0; i <= romanCharacters.length; i++) {
+        char = romanCharacters[i];
+        number = romanNumerals[romanCharacters[i]];
+
+        while (input - number >= 0) {
+            output += char;
+            input -= number;
+        }
+    }
+
+    return output;
 }
 
-convertToRoman(36);
-
-
-convertToRoman(2); // should return "II".
-convertToRoman(3); // should return "III".
-convertToRoman(4); // should return "IV".
-convertToRoman(5); // should return "V".
-convertToRoman(9); // should return "IX".
-convertToRoman(12); // should return "XII".
-convertToRoman(16); // should return "XVI".
-convertToRoman(29); // should return "XXIX".
-convertToRoman(44); // should return "XLIV".
-convertToRoman(45); // should return "XLV"
-convertToRoman(68); // should return "LXVIII"
-convertToRoman(83); // should return "LXXXIII"
-convertToRoman(97); // should return "XCVII"
-convertToRoman(99); // should return "XCIX"
-convertToRoman(500); // should return "D"
-convertToRoman(501); // should return "DI"
-convertToRoman(649); // should return "DCXLIX"
-convertToRoman(798); // should return "DCCXCVIII"
-convertToRoman(891); // should return "DCCCXCI"
-convertToRoman(1000); // should return "M"
-convertToRoman(1004); // should return "MIV"
-convertToRoman(1006); // should return "MVI"
-convertToRoman(1023); // should return "MXXIII"
-convertToRoman(2014); // should return "MMXIV"
-convertToRoman(3999); // should return "MMMCMXCIX"
+/* Test */
+// convertToRoman(2); // should return "II".
+// convertToRoman(3); // should return "III".
+// convertToRoman(4); // should return "IV".
+// convertToRoman(5); // should return "V".
+// convertToRoman(9); // should return "IX".
+// convertToRoman(12); // should return "XII".
+// convertToRoman(16); // should return "XVI".
+// convertToRoman(29); // should return "XXIX".
+// convertToRoman(44); // should return "XLIV".
+// convertToRoman(45); // should return "XLV"
+// convertToRoman(68); // should return "LXVIII"
+// convertToRoman(83); // should return "LXXXIII"
+// convertToRoman(97); // should return "XCVII"
+// convertToRoman(99); // should return "XCIX"
+// convertToRoman(500); // should return "D"
+// convertToRoman(501); // should return "DI"
+// convertToRoman(649); // should return "DCXLIX"
+// convertToRoman(798); // should return "DCCXCVIII"
+// convertToRoman(891); // should return "DCCCXCI"
+// convertToRoman(1000); // should return "M"
+// convertToRoman(1004); // should return "MIV"
+// convertToRoman(1006); // should return "MVI"
+// convertToRoman(1023); // should return "MXXIII"
+// convertToRoman(2014); // should return "MMXIV"
+// convertToRoman(3999); // should return "MMMCMXCIX"
