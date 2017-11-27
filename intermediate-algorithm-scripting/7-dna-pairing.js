09cea665be3cba98a581f774d815a9bdcc26f2e1
Mick <mpiereder@gmail.com>
2017-11-26 18:13:04 -0700

    finsihed dna pairing

--- intermediate-algorithm-scripting/7-dna-pairing.js
+++ intermediate-algorithm-scripting/7-dna-pairing.js
@@ -19,11 +19,26 @@ String.prototype.split()
 
 
 function pairElement(str) {
-    return str;
-  }
-  
-  pairElement("GCG");
-  
-  pairElement("ATCGA") // should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
-  pairElement("TTGAG") // should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
-  pairElement("CTCTA") // should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].  
+    var arr = str.split('');
+    var output = [];
+    var pairs = {
+        "G": "C",
+        "C": "G",
+        "A": "T",
+        "T": "A"
+    };
+
+    for (var i = 0; i < arr.length; i++) {
+        var char = arr[i];
+        var pair = [char, pairs[char]];
+        output.push(pair);
+    }
+
+    return output;
+}
+
+pairElement("GCG");
+
+pairElement("ATCGA") // should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+pairElement("TTGAG") // should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+pairElement("CTCTA") // should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
