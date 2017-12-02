e4cacf54215872970507a87158bca9ea713b619f
Mick <mpiereder@gmail.com>
2017-12-01 19:38:06 -0700

    finished convert to html

--- intermediate-algorithm-scripting/10-sorted-union.js
+++ intermediate-algorithm-scripting/10-sorted-union.js
@@ -30,8 +30,6 @@ function uniteUnique(arr) {
     return filtered;
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
-
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]); // should return [1, 3, 2, 5, 4].
 uniteUnique([1, 3, 2], [1, [5]], [2, [4]]); // should return [1, 3, 2, [5], [4]].
 uniteUnique([1, 2, 3], [5, 2, 1]); // should return [1, 2, 3, 5].
--- intermediate-algorithm-scripting/11-convert-html-entities.js
+++ intermediate-algorithm-scripting/11-convert-html-entities.js
@@ -1,8 +1,8 @@
 /*
-Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+Convert the characters &, <, >, " (double quote), and ' (apostrophe),
+in a string to their corresponding HTML entities.
 
 Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
-
 Here are some helpful links:
 
 RegExp
@@ -10,16 +10,19 @@ HTML Entities
 String.prototype.replace()
 */
 
-
 function convertHTML(str) {
-    // &colon;&rpar;
-    return str;
-  }
-  
-  convertHTML("Dolce & Gabbana") // should return Dolce &​amp; Gabbana.
-  convertHTML("Hamburgers < Pizza < Tacos") // should return Hamburgers &​lt; Pizza &​lt; Tacos.
-  convertHTML("Sixty > twelve") // should return Sixty &​gt; twelve.
-  convertHTML('Stuff in "quotation marks"') // should return Stuff in &​quot;quotation marks&​quot;.
-  convertHTML("Shindler's List") // should return Shindler&​apos;s List.
-  convertHTML("<>") // should return &​lt;&​gt;.
-  convertHTML("abc") // should return abc.
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/'/g, '&apos;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+
+convertHTML("Dolce & Gabbana") // should return Dolce &​amp; Gabbana.
+convertHTML("Hamburgers < Pizza < Tacos") // should return Hamburgers &​lt; Pizza &​lt; Tacos.
+convertHTML("Sixty > twelve") // should return Sixty &​gt; twelve.
+convertHTML('Stuff in "quotation marks"') // should return Stuff in &​quot;quotation marks&​quot;.
+convertHTML("Shindler's List") // should return Shindler&​apos;s List.
+convertHTML("<>") // should return &​lt;&​gt;.
+convertHTML("abc") // should return abc.
