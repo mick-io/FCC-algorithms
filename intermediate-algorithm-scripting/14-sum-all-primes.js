5c2e2de52799c43f6aa1ed615e2b08fdf25819c0
Michael Piereder <I21414@xactware.com>
2017-12-13 17:34:10 -0700

    finshed sum all primes

---
+++ intermediate-algorithm-scripting/14-sum-all-primes.js
@@ -0,0 +1,46 @@
+/*
+Sum all the prime numbers up to and including the provided number.
+
+A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+
+The provided number may not be a prime.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+For Loops
+Array.prototype.push()
+*/
+
+
+
+function sumPrimes(num) {
+    var output = 0;
+
+    for (var i = 2; i <= num; i++) {
+        if (isPrime(i)) {
+            output += i;
+        }
+    }
+
+    console.log(output);
+    return output;
+}
+
+function isPrime(value) {
+    var output = true;
+
+    for (var i = 2; i < value; i++) {
+        if (value % i === 0) {
+            output = false;
+            break;
+        }
+    }
+
+    return output;
+}
+
+sumPrimes(10) // should return a number.
+sumPrimes(10) // should return 17.
+sumPrimes(977) // should return 73156.
