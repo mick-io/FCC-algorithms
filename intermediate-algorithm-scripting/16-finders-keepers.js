ef6f3cc94de73935c8429eea7c19d49851039120
Michael Piereder <I21414@xactware.com>
2018-01-09 15:34:54 -0700

    finished

--- intermediate-algorithm-scripting/16-finders-keepers.js
+++ intermediate-algorithm-scripting/16-finders-keepers.js
@@ -8,11 +8,13 @@ Here are some helpful links:
 Array.prototype.filter()
 */
 
-
 function findElement(arr, func) {
-    var num = 0;
-    return num;
-  }
-  
-findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) // should return 8.
-findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) // should return undefined.
+    return arr.filter(func)[0];
+}
+
+findElement([1, 3, 5, 8, 9, 10], function (num) {
+    return num % 2 === 0;
+}) // should return 8.
+findElement([1, 3, 5, 9], function (num) {
+    return num % 2 === 0;
+}) // should return undefined.
