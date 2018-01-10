dc34c3bba1cc89c56f43844f2cc25ef61ae9f722
Michael Piereder <I21414@xactware.com>
2018-01-09 18:09:38 -0700

    updated

---
+++ intermediate-algorithm-scripting/21-arguments-optional.js
@@ -0,0 +1,48 @@
+/*
+Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+
+Calling this returned function with a single argument will then return the sum:
+
+var sumTwoAnd = addTogether(2);
+
+sumTwoAnd(3) returns 5.
+
+If either argument isn't a valid number, return undefined.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Closures
+Arguments object
+*/
+
+function addTogether() {
+    var firstArg = arguments[0];
+    var secondArg = arguments[1];
+    var output = 0;
+
+    var condition1 = typeof firstArg === "number";
+    var condition2 = typeof secondArg === "number";
+    var condition3 = typeof secondArg === undefined;
+
+
+    if (condition1 && condition2) {
+        output = firstArg + secondArg;
+    } else if (condition1 && condition3) {
+        output = function (value) {
+            return firstArg + value;
+        }
+    } else {
+        output = undefined
+    }
+    return output;
+}
+
+addTogether(2, 3) // should return 5.
+// addTogether(2)(3) // should return 5.
+addTogether("http://bit.ly/IqT6zt") // should return undefined.
+addTogether(2, "3") // should return undefined.
+// addTogether(2)([3]) // should return undefined.
