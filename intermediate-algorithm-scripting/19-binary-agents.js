8a35c03b258e140579fcce3c45ded4d28e2f140f
Michael Piereder <I21414@xactware.com>
2018-01-09 17:40:32 -0700

    finished

--- intermediate-algorithm-scripting/19-binary-agents.js
+++ intermediate-algorithm-scripting/19-binary-agents.js
@@ -12,7 +12,14 @@ String.fromCharCode()
 */
 
 function binaryAgent(str) {
-    return str;
+    var binaryArr = str.split(" ");
+    var outputString = "";
+
+    for (var i = 0; i < binaryArr.length; i++) {
+        var digit = parseInt(binaryArr[i], 2);
+        outputString += String.fromCharCode(digit);
+    }
+    return outputString;
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111") // should return "Aren't bonfires fun!?"
