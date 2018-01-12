5ddb031d7d6315674e383b39633be194d51c5267
Piereder, Michael <mwpiereder@verisk.com>
2018-01-12 15:34:39 -0700

    finished

--- advanced-algorithm-scripting/1-validate-us-tel-num.js
+++ advanced-algorithm-scripting/1-validate-us-tel-num.js
@@ -19,37 +19,37 @@ RegExp
 */
 
 function telephoneCheck(str) {
-    var nineDigitDashed = /[0-9]{3}-[0-9]{3}-[0-9]{4}/.test(str);
-
-
+    var telRegex = /^1?\s?[0-9]{3}[-|\s]?[0-9]{3}[-|\s]?[0-9]{4}$/;
+    var altTelRegex = /^1?\s?\([0-9]{3}\)[-|\s]?[0-9]{3}[-|\s]?[0-9]{4}$/;
+    
+    return telRegex.test(str) || altTelRegex.test(str);
 }
 
 
-/* TEST */
-telephoneCheck("555-555-5555") // should return a boolean.
+// /* TEST */
 telephoneCheck("1 555-555-5555") // should return true.
-// telephoneCheck("1 (555) 555-5555") // should return true.
-// telephoneCheck("5555555555") // should return true.
-// telephoneCheck("555-555-5555") // should return true.
-// telephoneCheck("(555)555-5555") // should return true.
-// telephoneCheck("1(555)555-5555") // should return true.
-// telephoneCheck("555-5555") // should return false.
-// telephoneCheck("5555555") // should return false.
-// telephoneCheck("1 555)555-5555") // should return false.
-// telephoneCheck("1 555 555 5555") // should return true.
-// telephoneCheck("1 456 789 4444") // should return true.
-// telephoneCheck("123**&!!asdf#") // should return false.
-// telephoneCheck("55555555") // should return false.
-// telephoneCheck("(6505552368)") // should return false
-// telephoneCheck("2 (757) 622-7382") // should return false.
-// telephoneCheck("0 (757) 622-7382") // should return false.
-// telephoneCheck("-1 (757) 622-7382") // should return false
-// telephoneCheck("2 757 622-7382") // should return false.
-// telephoneCheck("10 (757) 622-7382") // should return false.
-// telephoneCheck("27576227382") // should return false.
-// telephoneCheck("(275)76227382") // should return false.
-// telephoneCheck("2(757)6227382") // should return false.
-// telephoneCheck("2(757)622-7382") // should return false.
-// telephoneCheck("555)-555-5555") // should return false.
-// telephoneCheck("(555-555-5555") // should return false.
-// telephoneCheck("(555)5(55?)-5555") // should return false.
+telephoneCheck("1 (555) 555-5555") // should return true.
+telephoneCheck("5555555555") // should return true.
+telephoneCheck("555-555-5555") // should return true.
+telephoneCheck("(555)555-5555") // should return true.
+telephoneCheck("1(555)555-5555") // should return true.
+telephoneCheck("555-5555") // should return false.
+telephoneCheck("5555555") // should return false.
+telephoneCheck("1 555)555-5555") // should return false.
+telephoneCheck("1 555 555 5555") // should return true.
+telephoneCheck("1 456 789 4444") // should return true.
+telephoneCheck("123**&!!asdf#") // should return false.
+telephoneCheck("55555555") // should return false.
+telephoneCheck("(6505552368)") // should return false
+telephoneCheck("2 (757) 622-7382") // should return false.
+telephoneCheck("0 (757) 622-7382") // should return false.
+telephoneCheck("-1 (757) 622-7382") // should return false
+telephoneCheck("2 757 622-7382") // should return false.
+telephoneCheck("10 (757) 622-7382") // should return false.
+telephoneCheck("27576227382") // should return false.
+telephoneCheck("(275)76227382") // should return false.
+telephoneCheck("2(757)6227382") // should return false.
+telephoneCheck("2(757)622-7382") // should return false.
+telephoneCheck("555)-555-5555") // should return false.
+telephoneCheck("(555-555-5555") // should return false.
+telephoneCheck("(555)5(55?)-5555") // should return false.
