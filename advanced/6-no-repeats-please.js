1137324458107553f597ec3934b83e682ec9ed81
Mick Piereder <mpiereder@poulton.com>
2018-04-09 19:31:40 -0600

    started no repeat

--- advanced/6-no-repeats-please.js
+++ advanced/6-no-repeats-please.js
@@ -12,18 +12,32 @@ RegExp
 */
 
 function permAlone(str) {
-  return str;
+  var indices = str.length;
+  var letters = new Set(str.split("")).size;
+  var permutations = calcPermutations(indices, letters);
+
+  debugger;
 }
 
-permAlone("aab");
+function calcPermutations(objects, samples) {
+  return factorial(objects) / factorial(objects - samples);
+}
 
-permAlone("aab"); // should return a number.
+function factorial(num) {
+  var output = 1;
+  for (var i = num; i > 1; i--) {
+    output *= i;
+  }
+  return output;
+}
+
+/* TEST */
 permAlone("aab"); // should return 2.
-permAlone("aaa"); // should return 0.
+// permAlone("aaa"); // should return 0.
 permAlone("aabb"); // should return 8.
 permAlone("abcdefa"); // should return 3600.
 permAlone("abfdefa"); // should return 2640.
-permAlone("zzzzzzzz"); // should return 0.
-permAlone("a"); // should return 1.
-permAlone("aaab"); // should return 0.
+// permAlone("zzzzzzzz"); // should return 0.
+// permAlone("a"); // should return 1.
+// permAlone("aaab"); // should return 0.
 permAlone("aaabb"); // should return 12.
